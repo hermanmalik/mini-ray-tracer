739f7b626c8192dd128654e6dc058188b170715a
Herman Malik <hermanmalik@berkeley.edu>
2023-12-21 11:40:15 -0800

    reflect + refract methods

--- src/ThreeVector.java
+++ src/ThreeVector.java
@@ -2,6 +2,8 @@ public class ThreeVector {
     private final double x;
     private final double y;
     private final double z;
+    final double eta_i = 1.f; // for snell
+    final double eta_t = 1.0;
 
     public ThreeVector(double x, double y, double z) {
         this.x = x;
@@ -36,8 +38,8 @@ public class ThreeVector {
     public ThreeVector minus(ThreeVector other) {
         return new ThreeVector(x - other.x(), y - other.y(), z - other.z());
     }
-    public ThreeVector pointwiseTimes(ThreeVector other) {
-        return new ThreeVector(other.x() * x, other.y() * y, other.z() * z);
+    public double dot(ThreeVector other) {
+        return other.x() * x + other.y() * y + other.z() * z;
     }
     public ThreeVector cross(ThreeVector other) {
         return new ThreeVector(y * other.z() - z * other.y(), z * other.x() - x * other.z(),
@@ -45,6 +47,24 @@ public class ThreeVector {
     }
     public ThreeVector normalized() {
         return this.times(1 / this.norm());
+    }
+
+    ThreeVector reflect(ThreeVector I, ThreeVector N) {
+        return I.minus(N.times(2.f).times(I.dot(N))); //I - N*2.f*(I*N)
+    }
+
+    ThreeVector refract(ThreeVector I, ThreeVector N) { // Snell's law
+        double cosi = - Math.max(-1.f, Math.min(1.f, I.dot(N)));
+        if (cosi < 0) {
+            return refract(I, N.times(-1)); // if the ray comes from the inside the object, swap the air and the media
+        }
+        double eta = eta_i / eta_t;
+        double k = 1 - eta*eta*(1 - cosi*cosi);
+        if (k < 0) {
+            return new ThreeVector(1,0,0); // total reflection, no ray to refract
+        } else {
+            return I.times(eta).plus(N.times(eta*cosi - Math.sqrt(k)));
+        }
     }
 
 
